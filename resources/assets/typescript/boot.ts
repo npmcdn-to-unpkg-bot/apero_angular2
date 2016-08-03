38d8daa48f835c909dbd67a17959387308636a12
Antoine07 <antoine.lucsko@wanadoo.fr>
2016-08-03 13:54:30 +0200

    first commit

---
+++ resources/assets/typescript/boot.ts
@@ -0,0 +1,7 @@
+/// <reference path="../../../typings/browser.d.ts" />
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import {HTTP_PROVIDERS} from '@angular/http'
+
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent, [ HTTP_PROVIDERS]).catch(err => console.log(err));
